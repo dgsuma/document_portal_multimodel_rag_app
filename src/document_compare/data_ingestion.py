4154475a7d1ab2e9f77b04d25aaa0a8c70929718
dgsuma <dumindags@gmail.com>
2025-08-03 07:46:01 +1000

    complete skeleton with exception handling for file data_ingestion.py

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -5,23 +5,37 @@ from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 
 class DocumentIngestion:
-    def __init__(self):
-        pass
+    def __init__(self, base_dir:str="data\\document_compare"):
+        self.log = CustomLogger().get_logger(__name__)
+        self.base_dir = Path(base_dir)
+        self.base_dir.mkdir(parents=True, exist_ok=True)
     
     def delete_existing_files(self):
         """
         Deletes existing files at the specified paths.
         """
-        pass   
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error deleting existing files: {e}")
+            raise DocumentPortalException("An error occurred while deleting existing files.", sys)   
     
     def save_uploaded_files(self):
         """
         Saves uploaded files to a specific directory.
         """
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error saving uploaded files: {e}")
+            raise DocumentPortalException("An error occurred while saving uploaded files.", sys)
     
     def read_pdf(self):
         """
         Reads a PDF file and extracts text from each page.
         """
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error reading PDF: {e}")
+            raise DocumentPortalException("An error occurred while reading the PDF.", sys)
