faeda80f59dc41bd2de381d1738e4c3237f1d2eb
dgsuma <dumindags@gmail.com>
2025-08-03 07:54:10 +1000

    code updated for reading pdf

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -30,12 +30,22 @@ class DocumentIngestion:
             self.log.error(f"Error saving uploaded files: {e}")
             raise DocumentPortalException("An error occurred while saving uploaded files.", sys)
     
-    def read_pdf(self):
+    def read_pdf(self,pdf_path: Path)->str:
         """
         Reads a PDF file and extracts text from each page.
         """
         try:
-            pass
+            with fitz.open(pdf_path) as doc:
+                if doc.is_encrypted:
+                    raise ValueError(f"PDF is encrypted: {pdf_path.name}")
+                all_text = []
+                for page_num in range(doc.page_count):
+                    page = doc.load_page(page_num)
+                    text = page.get_text() 
+                    if text.strip():
+                        all_text.append(f"\n --- Page {page_num + 1} --- \n{text}")
+                self.log.info("PDF read successfully", file=str(pdf_path), pages=len(all_text))
+                return "\n".join(all_text)
         except Exception as e:
             self.log.error(f"Error reading PDF: {e}")
             raise DocumentPortalException("An error occurred while reading the PDF.", sys)
